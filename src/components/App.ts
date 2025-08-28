cd0d630f336a3f2eb63aad8a61e0cd34b58d0750
Rafael Aguiar <rafael.aguiar02@gmail.com>
2025-08-28 14:17:50 -0300

    small intro camera animation

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, MeshBuilder, Vector3, type EngineOptions, type SceneOptions, type Camera } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Animation, CubicEase, EasingFunction, MeshBuilder, Vector3, type EngineOptions, type SceneOptions, type Camera } from "@babylonjs/core";
 import BuildingSpawner from "./objects/BuildingSpawner";
 
 import MeshLoader from "./singletons/MeshLoader";
@@ -29,14 +29,40 @@ export default class App {
         MeshLoader.loadAssets();
 
         WorldEnvironment.setup(this.scene);
-
-        this.mainCamera = new ArcRotateCamera("MainCamera", Math.PI / 2, Math.PI / 2.1, 100, new Vector3(0, 10, 0), this.scene);
+        
+        const cameraInitialHeight = 80;
+        this.mainCamera = new ArcRotateCamera("MainCamera", Math.PI / 2, Math.PI / 2.1, 100, new Vector3(0, cameraInitialHeight, 0), this.scene);
         //this.mainCamera.attachControl(this.scene, true);
 
         this.scene.onReadyObservable.addOnce(() => {
             const buildingSpawner = new BuildingSpawner(this.scene);
 
             const ground = MeshBuilder.CreateGround("ground", { width: 200, height: 200 }, this.scene);
+
+            const framerate = 30;
+
+            const lowerCamera = new Animation("lowerCamera", "target.y", framerate, Animation.ANIMATIONTYPE_FLOAT);
+            const lowerCameraDuration = 8;
+
+            const keyFrames = [{
+                frame: 0,
+                value: cameraInitialHeight,
+            }, {
+                frame: lowerCameraDuration * framerate,
+                value: 10,
+            }];
+
+            lowerCamera.setKeys(keyFrames);
+
+            const easingFn = new CubicEase();
+            easingFn.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT);
+            lowerCamera.setEasingFunction(easingFn);
+
+            this.mainCamera.animations.push(lowerCamera);
+
+            setTimeout(() => {
+                this.scene.beginAnimation(this.mainCamera, 0, lowerCameraDuration * framerate, false);
+            }, 2000);
         });
 
         if (window) {
