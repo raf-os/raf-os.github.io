6dedfe30cacbfdd9ab6afaf08a46cd0e84abec3d
Rafael Aguiar <rafael.aguiar02@gmail.com>
2025-09-23 13:53:27 -0300

    fuck off m8

--- src/app/sections/Experiences.tsx
+++ src/app/sections/Experiences.tsx
@@ -1,21 +1,21 @@
 'use client';
 
-export default function ExperiencesPage() {
-    return (
-        <div>
-            <h1>Experience section</h1>
-        </div>
-    )
-}
+// export default function ExperiencesPage() {
+//     return (
+//         <div>
+//             <h1>Experience section</h1>
+//         </div>
+//     )
+// }
 
-type ExperienceItemProps = {
-    title: string
-}
+// type ExperienceItemProps = {
+//     title: string
+// }
 
-function ExperienceItem() {
-    return (
-        <div>
-            <h2></h2>
-        </div>
-    )
-}
+// function ExperienceItem() {
+//     return (
+//         <div>
+//             <h2></h2>
+//         </div>
+//     )
+// }
